bca8fcd3d595950076ab8e6da24c9c307d117e36
Alexander Tolich <aleksandrtolich@gmail.com>
2018-11-10 15:22:58 +0300

    added task 4 (found consumers which need the storage)

---
+++ src/com/traning/task4/AlgFoundCosumersWithStorage.java
@@ -0,0 +1,43 @@
+package com.traning.task4;
+
+import com.traning.task4.structures.Model;
+import com.traning.task4.structures.Solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlgFoundCosumersWithStorage {
+    private Model m;
+
+    public AlgFoundCosumersWithStorage(Model m) {
+        this.m = m;
+    }
+
+    public Solution solve() {
+        int maxStorage = m.getSumA();
+        GraphNet g = GraphUtils.getBasicWithStorageGraphStructureFromModel(m, maxStorage);
+        Solution solution = null;
+        List<Integer> listConsumers = new ArrayList<>();
+        for(int i = 0; i < m.getM(); i++){
+            GraphUtils.deleteStorageBy(g, m, i);
+            solution = new AlgFordaFalc(g).solve();
+            if(solution.getUpperBoundForMaxFlow() != solution.getMaxFlow()){
+                GraphUtils.addStorageBy(g, m, i, maxStorage);
+                listConsumers.add(i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for(Integer i : listConsumers){
+            sb.append(i);
+            if(!listConsumers.get(listConsumers.size() - 1).equals(i)){
+                sb.append(", ");
+            }
+        }
+        sb.append("}");
+        if (solution != null) {
+            solution.addParam("numConsumers", sb.toString());
+        }
+        return solution;
+    }
+}
