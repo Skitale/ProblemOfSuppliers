55018089e3ecff10d1d6eac9d43bed734e148e1b
Alexander Tolich <aleksandrtolich@gmail.com>
2018-11-10 15:21:33 +0300

    added task 3 (found min storage for all consumers)

---
+++ src/com/traning/task4/AlgFoundMinStorage.java
@@ -0,0 +1,34 @@
+package com.traning.task4;
+
+import com.traning.task4.structures.Model;
+import com.traning.task4.structures.Solution;
+
+public class AlgFoundMinStorage {
+    private Model m;
+
+    public AlgFoundMinStorage(Model m) {
+        this.m = m;
+    }
+
+    public Solution solve() {
+        int rightBound = m.getSumA();
+        int leftBound = 0;
+        int mid = 0;
+        int key = m.getSumS();
+        GraphNet g = GraphUtils.getBasicWithStorageGraphStructureFromModel(m, rightBound);
+        Solution result = null;
+        while (!(leftBound >= rightBound)) {
+            mid = leftBound + (int)((double)(rightBound - leftBound) / 2d);
+            GraphUtils.changeStotageForAllConsumers(m, g, mid);
+            Solution tmpSolution = new AlgFordaFalc(g).solve();
+            if(tmpSolution.getMaxFlow() == key){
+                tmpSolution.addParam("e", mid);
+                result = tmpSolution;
+                rightBound = mid;
+            } else {
+                leftBound = mid + 1;
+            }
+        }
+        return result;
+    }
+}
